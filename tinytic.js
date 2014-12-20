5146c452740e0df1f7f6e2deb5fc313987b2b953
benji6 <benhall22@hotmail.co.uk>
2014-12-20 09:48:46 +0000

    tinytic now takes a maximum time difference argument

--- tinytic.js
+++ tinytic.js
@@ -1,10 +1,14 @@
 var then = new Date().getTime();
 var now = new Date().getTime();
 
-var toc = function() {
+var toc = function(maxDT) {
 	then = now;
 	now = new Date().getTime();
-	return now - then;
+	var dT = now - then;
+	if (maxDT && maxDT < dT) {
+		return maxDT;
+	}
+	return dT;
 };
 
 exports.toc = toc;
