dce1ea23402cf23f6a68903cb88aa1844f2cba33
benji6 <benhall22@hotmail.co.uk>
2015-01-03 10:39:15 +0000

    added method for obtaining total elapsed time

--- tinytic.js
+++ tinytic.js
@@ -1,11 +1,19 @@
-var then = new Date().getTime();
+var firstTime = new Date().getTime();
+var then = firstTime;
 var now = new Date().getTime();
 
 module.exports.toc = function(maxDT) {
 	then = now;
 	now = new Date().getTime();
 	var dT = now - then;
-	if (maxDT !== undefined && maxDT < dT) {
+	if (maxDT < dT) {
+		return maxDT;
+	}
+	return dT;
+};
+module.exports.total = function(maxDT) {
+	var dT = now - firstTime;
+	if (maxDT < dT) {
 		return maxDT;
 	}
 	return dT;
