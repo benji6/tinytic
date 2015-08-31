5a931c8846e9d610318ba0eb5b9dfbafc2c3d7aa
benji6 <benhall22@hotmail.co.uk>
2015-08-31 18:32:21 +0100

    spaces, not tabs

--- tinytic.js
+++ tinytic.js
@@ -2,8 +2,8 @@ var tinytic = (function() {
   var getNow = typeof performance === "object" && typeof performance.now === "function" && performance.now.bind(performance) ||
     Date.now ||
     function () {
-			return new Date().getTime();
-		};
+      return new Date().getTime();
+    };
 
   var t0 = getNow();
   var then = t0;
@@ -13,12 +13,12 @@ var tinytic = (function() {
     then = now;
     now = getNow();
     var dT = now - then;
-		return maxDT < dT ? maxDT : dT;
+    return maxDT < dT ? maxDT : dT;
   };
 
   var total = function (maxDT) {
     var dT = getNow() - t0;
-		return maxDT < dT ? maxDT : dT;
+    return maxDT < dT ? maxDT : dT;
   };
 
   var reset = function () {
